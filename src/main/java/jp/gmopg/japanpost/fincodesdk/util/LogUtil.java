4a8c65095ffecabb315ce63e7006e23cda281547
fincode-developer <fincode-developer@gmo-pg.com>
2022-04-13 10:13:05 +0900

    First Commit

---
+++ src/main/java/jp/gmopg/japanpost/fincodesdk/util/LogUtil.java
@@ -0,0 +1,9 @@
+package jp.gmopg.japanpost.fincodesdk.util;
+
+import android.util.Log;
+
+public class LogUtil {
+    public static void info(String message) {
+        Log.i("INFO", message);
+    }
+}
