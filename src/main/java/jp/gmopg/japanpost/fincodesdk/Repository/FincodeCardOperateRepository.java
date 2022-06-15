073ef0c18f93e974a08712de273f4bfffa874277
fincode-developer <fincode-developer@gmo-pg.com>
2022-06-15 17:21:18 +0900

    add card info api

--- src/main/java/jp/gmopg/japanpost/fincodesdk/Repository/FincodeCardOperateRepository.java
+++ src/main/java/jp/gmopg/japanpost/fincodesdk/Repository/FincodeCardOperateRepository.java
@@ -1,17 +1,55 @@
 package jp.gmopg.japanpost.fincodesdk.Repository;
 
-import jp.gmopg.japanpost.fincodesdk.enumeration.APIEndpoint;
+import java.util.HashMap;
+
+import jp.gmopg.japanpost.fincodesdk.api.AsyncHttpClient;
+import jp.gmopg.japanpost.fincodesdk.api.CardOperateApiInterface;
+import jp.gmopg.japanpost.fincodesdk.api.FincodeCallback;
+import jp.gmopg.japanpost.fincodesdk.entities.api.FincodeCardInfoListResponse;
+import jp.gmopg.japanpost.fincodesdk.util.HttpUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
- * Created by a.nakajima on 2022/06/13.
+ * Created by a.nakajima on 2022/06/15.
  */
-public class FincodeCardOperateRepository {
+public class FincodeCardOperateRepository<T> {
 
-    public void cardInfoList() {
-        String endpoint = APIEndpoint.CARD_INFO_LIST.getEndPoint();
+    private static FincodeCardOperateRepository instance;
 
-
-
-
+    private FincodeCardOperateRepository() {
     }
+
+    public static FincodeCardOperateRepository getInstance() {
+        if(instance == null) {
+            instance = new FincodeCardOperateRepository();
+        }
+        return instance;
+    }
+
+    public void getCardInfoList(HashMap<String, String> header, String customerId, FincodeCallback<FincodeCardInfoListResponse> fincodeCallback) {
+        CardOperateApiInterface api = AsyncHttpClient.getInstance().getAsyncHttpClient(CardOperateApiInterface.class);
+
+        api.cardInfoList(header, customerId)
+                .enqueue(new Callback<FincodeCardInfoListResponse>() {
+                    @Override
+                    public void onResponse(Call<FincodeCardInfoListResponse> call, Response<FincodeCardInfoListResponse> response) {
+                        if(response.isSuccessful()) {
+                            fincodeCallback.onResponse(response.body());
+                        } else {
+                            fincodeCallback.onFailure(HttpUtil.getErrorInfo(response));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<FincodeCardInfoListResponse> call, Throwable t) {
+                        // do nothing
+                    }
+                });
+    }
+
+    // TODO カード登録APIの実装する
+
+    // TODO カード更新APIの実装する
 }
